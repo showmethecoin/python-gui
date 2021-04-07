fbda21df5ab046c147af412badedf7d90b2d7926
유찬양 <clsrnthd7@gmail.com>
2021-04-08 00:31:39 +0900

    feat: 실시간 차트 구현
    - 위젯간 연결
    - 실시간 차트 및 호가창 구현

---
+++ showmethecoin.py
@@ -0,0 +1,20 @@
+
+import sys
+import platform
+from test import Ui_MainWin
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWin()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ test_orderbook.py
@@ -0,0 +1,158 @@
+
+import sys
+import time
+import PySide2
+import pyupbit
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QTableWidgetItem, QProgressBar
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QPropertyAnimation
+
+# from pyside_dynamic import loadUi
+# from PySide2.QtUiTools import QUiLoader
+# from PySide2.QtWidgets import QMainWindow, QWidget
+# from PySide2.QtWidgets import QTableWidgetItem, QProgressBar
+# from PySide2.QtCore import QFile, Qt, QThread, QPropertyAnimation, Signal
+
+class OrderbookWorker(QThread):
+    dataSent = pyqtSignal(list)
+
+    def __init__(self, ticker):
+        super().__init__()
+        self.size = 10
+        self.ticker = ticker
+        self.alive = True
+
+    def run(self):
+        while self.alive:
+            data = pyupbit.get_orderbook("KRW-"+self.ticker)
+            time.sleep(0.05)
+            if data != None:
+                self.dataSent.emit(data)
+
+    def close(self):
+        self.alive = False
+
+class OrderbookWidget(QWidget):
+    def __init__(self, parent=None, ticker="BTC"):
+        super().__init__(parent)
+        uic.loadUi("orderbook.ui", self)
+
+        
+        #loadUi("orderbook.ui", self)
+        self.ticker = ticker
+
+        self.asksAnim = [ ]
+        self.bidsAnim = [ ]
+        
+
+        for i in range(self.tableBids.rowCount()):
+            # 매도호가
+            item_0 = QTableWidgetItem(str(""))
+            item_0.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableAsks.setItem(i, 0, item_0)
+
+            item_1 = QTableWidgetItem(str(""))
+            item_1.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableAsks.setItem(i, 1, item_1)
+
+            item_2 = QProgressBar(self.tableAsks)
+            item_2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            item_2.setStyleSheet("""
+                QProgressBar {background-color : rgba(0, 0, 0, 0%);border : 1}
+                QProgressBar::Chunk {background-color : rgba(255, 0, 0, 50%);border : 1}
+            """)
+            self.tableAsks.setCellWidget(i, 2, item_2)
+            anim = QPropertyAnimation(item_2, b"value")
+            anim.setDuration(200)
+            self.asksAnim.append(anim)
+
+            # 매수호가
+            item_0 = QTableWidgetItem(str(""))
+            item_0.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableBids.setItem(i, 0, item_0)
+
+            item_1 = QTableWidgetItem(str(""))
+            item_1.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.tableBids.setItem(i, 1, item_1)
+
+            item_2 = QProgressBar(self.tableBids)
+            item_2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            item_2.setStyleSheet("""
+                QProgressBar {background-color : rgba(0, 0, 0, 0%);border : 1}
+                QProgressBar::Chunk {background-color : rgba(0, 255, 0, 40%);border : 1}
+            """)
+            self.tableBids.setCellWidget(i, 2, item_2)
+            anim = QPropertyAnimation(item_2, b"value")
+            anim.setDuration(200)
+            self.bidsAnim.append(anim)
+
+        self.ow = OrderbookWorker(self.ticker)
+        self.ow.dataSent.connect(self.updateData)
+        self.ow.start()
+
+    def updateData(self, data):
+        #print(data[0]['orderbook_units'])
+        data = data[0]['orderbook_units']
+        # 아래 코드에서 적용될 수 있게 data 형태를 바꿔줌
+        data_dict = {}
+
+        # 먼저 리스트로 변경하고 나중에 dict에 추가
+        asks_tmp = []
+        bids_tmp = []
+
+        for i in range(10):
+            asks_tmp.append( { 'price' : data[i]['ask_price'], 'quantity' : data[i]['ask_size'] } )
+            bids_tmp.append( { 'price' : data[i]['bid_price'], 'quantity' : data[i]['bid_size'] } )
+        
+        data_dict['asks'] = asks_tmp
+        data_dict['bids'] = bids_tmp
+        
+        data = data_dict
+        
+
+        tradingValues = [ ]
+        for v in data['bids']:
+            tradingValues.append(int(v['price'] * v['quantity']))
+        maxtradingValue = max(tradingValues)
+        #print(maxtradingValue)
+
+        for i, v in enumerate(data['asks'][::-1]):
+            item_0 = self.tableAsks.item(i, 0)
+            item_0.setText(f"{v['price']:,}")
+            item_1 = self.tableAsks.item(i, 1)
+            item_1.setText(f"{v['quantity']:,}")
+            item_2 = self.tableAsks.cellWidget(i, 2)
+            item_2.setRange(0, maxtradingValue)
+            item_2.setFormat(f"{tradingValues[i]:,}")
+            self.asksAnim[i].setStartValue(item_2.value() if item_2.value() > 0 else 0)
+            self.asksAnim[i].setEndValue(tradingValues[i])
+            self.asksAnim[i].start()
+
+        for i, v in enumerate(data['bids']):
+            item_0 = self.tableBids.item(i, 0)
+            item_0.setText(f"{v['price']:,}")
+            item_1 = self.tableBids.item(i, 1)
+            item_1.setText(f"{v['quantity']:,}")
+            item_2 = self.tableBids.cellWidget(i, 2)
+            item_2.setRange(0, maxtradingValue)
+            item_2.setFormat(f"{tradingValues[i]:,}")
+            self.bidsAnim[i].setStartValue(item_2.value() if item_2.value() > 0 else 0)
+            self.bidsAnim[i].setEndValue(tradingValues[i])
+            self.bidsAnim[i].start()
+        
+
+    def closeEvent(self, event):
+        self.ow.close()
+
+
+if __name__ == "__main__":
+    import sys
+    #from PyQt5.QtWidgets import QApplication
+    from PySide2.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    ow = OrderbookWidget()
+    ow.show()
+    #cw = test_chart_list.ChartlistWidget()
+    #cw.show()
+    exit(app.exec_())
